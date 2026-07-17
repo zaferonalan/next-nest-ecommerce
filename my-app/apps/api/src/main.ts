5f8c97893e92bde467a88ec9c752a3e8fb3cdc2b
Zafer Günay Önalan <zafergunay7@gmail.com>
2026-07-17 22:24:12 +0300

    refactor: Port ayarı düzenlendi

--- my-app/apps/api/src/main.ts
+++ my-app/apps/api/src/main.ts
@@ -6,13 +6,16 @@
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import { ConfigService } from "@nestjs/config";
 
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3000;
+  const configservice = app.get(ConfigService)
+
+  const port = configservice.getOrThrow<number>("PORT");
   await app.listen(port);
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`,
